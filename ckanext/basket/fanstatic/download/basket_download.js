73e71ab346bcf7dd47037ddf5a55d4f5654165b6
sureL89 <georg.seyerl@gmail.com>
2018-09-24 17:26:47 +0200

    Use jquery plugin datatables. Still bug with filtered list (use Search and select downloadable package)

--- ckanext/basket/fanstatic/download/basket_download.js
+++ ckanext/basket/fanstatic/download/basket_download.js
@@ -10,21 +10,38 @@ ckan.module('basket_download', function ($) {
         var options = this.options;
         $.proxyAll(this, /_on/);
         // Add click event
+        $(':checkbox:checked').prop('checked',false);
+        var table = $('#tab-basket').DataTable({"ordering": false});
         document.getElementById('js_action.download').addEventListener('click', function(e){
             self._DownloadResources();
         });
       },
 
       _DownloadResources: function() {
+          var table = $('#tab-basket').DataTable();
+          var sel_pkg_ids = $.map(table.rows('.table-selected').ids(), function (item) {
+              return item;
+          });
+
+          var results = this.options.rscs_list.filter(
+              function(e) {
+                  return e.indexOf(this) > -1;
+              },
+              sel_pkg_ids
+          );
+
+          console.log(sel_pkg_ids);
+          console.log(results);
+
+          // Open download dialogues for selected resources
           var link = document.createElement('a');
 
           link.setAttribute('download', null);
           link.style.display = 'none';
-
           document.body.appendChild(link);
 
-          for (var i = 0; i < this.options.rscs_list.length; i++) {
-              link.setAttribute('href', this.options.rscs_list[i]);
+          for (var i = 0; i < results.length; i++) {
+              link.setAttribute('href', results[i]);
               link.click();
           }
           document.body.removeChild(link);
